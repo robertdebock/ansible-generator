30993b64156d7fe2fcf93c17d24e058fd5cddaa8
Robert de Bock <robert@meinit.nl>
2019-12-10 07:14:52 +0100

    Remove modules from README.md, they add no value.

--- scripts/modules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "---" ; cat tasks/* handlers/* | grep -A1 -E '(    )?^- name:' | grep -e '^  ' | grep -v 'block' | cut -d: -f1 | sort | uniq | while read module ; do echo "- ${module}" ; done
